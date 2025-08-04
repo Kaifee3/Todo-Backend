ed72d8a634056daed4cc42e5bf69eb36a8ac920d
Kaifee3 <kaifiazam138@gmail.com>
2025-08-04 14:16:41 +0530

    update

--- models/taskModel.js
+++ models/taskModel.js
@@ -5,6 +5,7 @@ const taskSchema = mongoose.Schema(
     title: { type: String, required: true },
     description: { type: String },
     status: { type: String, default: "Pending" },
+    isConfirmed: { type: Boolean, default: false }, // ✅ NEW FIELD
     email: { type: String, required: true },
     userId: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
   },
